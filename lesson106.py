5287ffe851368d22d34b9756bdb80a923e49ece4
Mark Peters <mapeters101@earthlink.net>
2025-03-11 17:29:39 -0400

    Work on lesson106 from 2:30 to 3:15.

--- lesson106.py
+++ lesson106.py
@@ -10,4 +10,14 @@ print(next(iterator))
 #print(next(iterator))
 print()
 
+iterator = iter(l)
+print(type(iterator))
+print(id(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+#print(next(iterator))
+#print(next(iterator))
+print()
+
 
