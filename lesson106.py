5270b0c49ddef1cb61c75465d48058502ce5468c
Mark Peters <mapeters101@earthlink.net>
2025-03-11 17:28:44 -0400

    Work on lesson106 to 2:30.

---
+++ lesson106.py
@@ -0,0 +1,13 @@
+l = [1,2,3]
+
+iterator = iter(l)
+print(type(iterator))
+print(id(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+#print(next(iterator))
+#print(next(iterator))
+print()
+
+
